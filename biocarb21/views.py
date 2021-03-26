a1aab92b7e0c00ee6ccd325ee60b6dfc19faee4d
brahian <brahianmr1997@gmail.com>
2021-03-26 14:46:07 -0500

    se dejo los dos formularios

--- biocarb21/views.py
+++ biocarb21/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render, redirect
 from .models import Userbc21
-from .forms import Userbc21Form
+from .forms import Userbc21Form1, Userbc21Form2
+
+
 
 def registrate(request):
     if request.method == "POST":
-        form = Userbc21Form(request.POST)
-        print(form)
+        form = Userbc21Form1(request.POST)
         if form.is_valid():
             user = form.save()
             return redirect('registrate-2', user.id)
     else:
-        form = Userbc21Form()
+        form = Userbc21Form1()
     context = {'form': form}
     return render(request, 'biocarb21\\registrate.html', context)
 
 def registrate_2(request, user_id):
     user = Userbc21.objects.get(id = user_id)
     if request.method == "POST":
-        form = Userbc21Form(request.POST, instance = user)
-        print(form)
+        form = Userbc21Form2(request.POST, instance = user)
         if form.is_valid():
             form.save()
             return redirect('registrate-exito', user.id)
     else:
-        form = Userbc21Form(instance = user)
+        form = Userbc21Form2(instance = user)
     context = {'form' : form}
     return render(request, 'biocarb21\\registrate-2.html', context)
 
