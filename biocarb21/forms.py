22724a3610ab4499dc83ad116e10e47a770549c5
brahian <brahianmr1997@gmail.com>
2021-03-27 11:52:03 -0500

    Buen trabajo con documentos de prueba

--- biocarb21/forms.py
+++ biocarb21/forms.py
@@ -17,10 +17,10 @@ class Userbc21Form2(forms.ModelForm):
         #widgets = {'fecha_nacimiento': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),}
         #'fecha_nacimiento':forms.SelectDateWidget(years=range(1900,2021)),
         widgets = {
-        'fecha_nacimiento':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Fecha de nacimiento:', 'onfocus':"this.type='date'"}),
-        'genero':forms.Select(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Fecha de nacimiento:'}),
-        'entidad_organizacion':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Entidad/Organización:'}),
-        'cargo':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Cargo:'}),
-        'departamento':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Departamento:'}),
-        'municipio':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Municipio:'}),
+        'fecha_nacimiento':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Fecha de nacimiento:', 'onfocus':"this.type='date'", 'required':'true'}),
+        'genero':forms.Select(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'genero:', 'required':'true'}),
+        'entidad_organizacion':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Entidad/Organización:', 'required':'true'}),
+        'cargo':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Cargo:', 'required':'true'}),
+        'departamento':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Departamento:',  'required':'true'}),
+        'municipio':forms.TextInput(attrs={'class': 'form-control mb-3 input-verde', 'placeholder': 'Municipio:', 'required':'true'}),
         }
