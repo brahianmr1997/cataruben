62aa157f3b80f0fc8fa1e358a426acbbcf91137a
brahian <brahianmr1997@gmail.com>
2021-03-28 17:24:30 -0500

    mapa coordenada y mensaje

--- mapjs/static/js/mapa.js
+++ mapjs/static/js/mapa.js
@@ -1,6 +1,34 @@
 
+var tilesProvider = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
-const tilesProvider = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-const mapa = L.map('mapa').setView([4.38, -74.09], 5)
+var mapa = L.map('mapa').setView([4.38, -74.09], 5)
 L.tileLayer(tilesProvider,{maZoom:18,}).addTo(mapa)
+
+var circle = L.circle([4.38, -74.09], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 5000
+}).addTo(mapa).bindPopup("I am a circle.");
+
+var marker = L.marker([4, -72.09]).addTo(mapa).bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup()
+
+var polygon = L.polygon([
+    [4 , -72],
+    [4.3, -71],
+    [4.5, -73]
+]).addTo(mapa).bindPopup("I am a polygon.");
+
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mapa);
+}
+
+mapa.on('click', onMapClick);
+
+
