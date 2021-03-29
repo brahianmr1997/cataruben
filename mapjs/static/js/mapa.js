c6b045790c4316d107337260b142068b566d9f72
brahian <brahianmr1997@gmail.com>
2021-03-29 12:20:13 -0500

    cali imagen

--- mapjs/static/js/mapa.js
+++ mapjs/static/js/mapa.js
@@ -4,6 +4,20 @@ var tilesProvider = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 var mapa = L.map('mapa').setView([4.38, -74.09], 5)
 L.tileLayer(tilesProvider,{maZoom:18,}).addTo(mapa)
 
+
+
+
+var cali = L.marker([3.45, -76.533])
+            .addTo(mapa)
+            .bindPopup("<h1>Cali</h1><p>Capital valle del cauca</p> <img src='http://www.institutocanzion.com.co/wp-content/uploads/2019/02/CALI.jpg' width='180' height='75'> ")
+            .openPopup()
+
+
+L.geoJSON(colombia, {
+    color: 'purple',
+}).addTo(mapa);
+
+
 var circle = L.circle([4.38, -74.09], {
     color: 'red',
     fillColor: '#f03',
@@ -11,7 +25,7 @@ var circle = L.circle([4.38, -74.09], {
     radius: 50000
 }).addTo(mapa).bindPopup("I am a circle.");
 
-var marker = L.marker([4, -69.09]).addTo(mapa).bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup()
+var marker = L.marker([4, -69.09]).addTo(mapa).bindPopup("<b>Hola colombia</b><br>Comentario de brahian munar")
 
 var polygon = L.polygon([
     [4 , -72],
